3ab0c3c79402b3179d342d45c5c5a96194246824
Gustavo Capelli <gustavocapelli20092005@gmail.com>
2024-12-27 11:24:04 -0300

    up

--- Aulas/jquerry/galeria de fotos/main.js
+++ Aulas/jquerry/galeria de fotos/main.js
@@ -10,13 +10,15 @@ $(document).ready(function(){
     $('form').on('submit', function(e){
         e.preventDefault();
         const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
-        const novoItem = $('<li></li>');
-        $(`<img src="$(enderecoDaNovaImagem)" />`).appendTo(novoItem);
+        const novoItem = $('<li style="display: none"></li>');
+        $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem);
         $(
             `<div class="overlay-imagem-link">
-            <a href="$("enderecoDaNovaImagem" target="_blank" title="Ver imagem em tamanho real"
+            <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real"</a>
             </div>`
         ).appendTo(novoItem);
         $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(1000);
+        $('#endereco-imagem-nova').val('')
     })
 })
